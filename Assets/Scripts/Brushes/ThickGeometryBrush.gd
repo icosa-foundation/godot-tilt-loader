a8a2ccf61acf7dedcbb81d0a10b8e43d2528d05b
Andy Baker <andy@andybak.net>
2026-06-17 14:53:25 +0100

    Port thick geometry brush to GDScript

---
+++ Assets/Scripts/Brushes/ThickGeometryBrush.gd
@@ -0,0 +1,278 @@
+class_name ThickGeometryBrush
+extends GeometryBrush
+
+const K_VERTS_IN_SOLID := 12
+const K_SHARED_VERTS_IN_SOLID_PAIR := 6
+const K_TRIS_IN_SOLID := 8
+const K_SHARED_TRIS_IN_SOLID_PAIR := 0
+
+const BRT := 0
+const BRB := 1
+const BMT := 2
+const BMB := 3
+const BLT := 4
+const BLB := 5
+const FRT := 6
+const FRB := 7
+const FMT := 8
+const FMB := 9
+const FLT := 10
+const FLB := 11
+
+enum UVStyle {
+	DISTANCE,
+	STRETCH,
+}
+
+const K_SOLID_MIN_LENGTH_METERS_PS := 0.002
+const K_MIN_MOVE_LENGTH_METERS_PS := 5e-4
+const K_BREAK_ANGLE_SCALAR := 2.0
+const K_SOLID_ASPECT_RATIO := 0.2
+
+var m_TextureEdgeChop := 0.1
+var m_uvStyle := UVStyle.DISTANCE
+
+func _init() -> void:
+	setup_geometry_brush(true, K_VERTS_IN_SOLID, false)
+
+func init_brush(desc: BrushDescriptor, local_pointer_xf: TrTransform) -> void:
+	super.init_brush(desc, local_pointer_xf)
+	m_geometry.set_layout(get_vertex_layout(desc))
+
+func get_vertex_layout(_desc: BrushDescriptor) -> GeometryPool.VertexLayout:
+	return GeometryPool.VertexLayout.create(
+		GeometryPool.TexcoordInfo.create(2, GeometryPool.Semantic.XY_IS_UV),
+		GeometryPool.TexcoordInfo.create(),
+		null,
+		true,
+		true,
+		true
+	)
+
+func get_spawn_interval(pressure01: float) -> float:
+	return K_SOLID_MIN_LENGTH_METERS_PS * App.METERS_TO_UNITS * pointer_to_local() + pressured_size(pressure01) * K_SOLID_ASPECT_RATIO
+
+func control_points_changed(knot_index: int) -> void:
+	on_changed_frame_knots(knot_index)
+	resize_geometry()
+	var start := knot_index - 1 if m_knots[knot_index - 1].has_geometry() else knot_index
+	on_changed_make_verts_and_normals(start)
+	if m_uvStyle == UVStyle.STRETCH:
+		on_changed_stretch_uvs(knot_index)
+	else:
+		on_changed_distance_uvs(knot_index)
+	on_changed_tangents(start)
+
+func on_changed_frame_knots(knot_index: int) -> void:
+	var min_move := K_MIN_MOVE_LENGTH_METERS_PS * App.METERS_TO_UNITS * pointer_to_local()
+	var prev := m_knots[knot_index - 1]
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		var should_break := false
+		var move := cur.point.m_Pos - prev.point.m_Pos
+		cur.length = move.length()
+		if cur.length < min_move:
+			should_break = true
+
+		if not should_break:
+			var move_normal := move / cur.length
+			var preferred_right := (Basis(cur.point.m_Orient) * Vector3.FORWARD * -1.0).cross(move_normal) if m_Desc.m_BackIsInvisible else prev.nRight
+			var frame := BaseBrushScript.compute_surface_frame_new(preferred_right, move_normal, cur.point.m_Orient)
+			cur.nRight = frame.right
+			cur.nSurface = frame.normal
+
+		if not should_break and prev.has_geometry():
+			var width_height_ratio := cur.length / pressured_size(cur.smoothedPressure)
+			var break_angle := rad_to_deg(atan(width_height_ratio)) * K_BREAK_ANGLE_SCALAR
+			var previous_move := prev.point.m_Pos - m_knots[index - 2].point.m_Pos
+			if rad_to_deg(previous_move.angle_to(move)) > break_angle:
+				should_break = true
+
+		if should_break:
+			cur.iTri = prev.iTri + prev.nTri
+			cur.nTri = 0
+			cur.iVert = prev.iVert + prev.nVert
+			cur.nVert = 0
+			cur.nRight = Vector3.ZERO
+			cur.nSurface = Vector3.ZERO
+		else:
+			cur.iTri = prev.iTri + prev.nTri
+			cur.iVert = prev.iVert + prev.nVert
+			if prev.has_geometry():
+				cur.iTri -= K_SHARED_TRIS_IN_SOLID_PAIR * NS
+				cur.iVert -= K_SHARED_VERTS_IN_SOLID_PAIR * NS
+			cur.nTri = K_TRIS_IN_SOLID * NS
+			cur.nVert = K_VERTS_IN_SOLID * NS
+
+		m_knots[index] = cur
+		prev = cur
+
+func on_changed_make_verts_and_normals(knot_index: int) -> void:
+	var belly := 1.0 / 8.0
+	var hypotenuse := sqrt(1.0 + belly * belly)
+	var sin_theta := belly / hypotenuse
+	var cos_theta := 1.0 / hypotenuse
+	var prev := m_knots[knot_index - 1]
+
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		if cur.has_geometry():
+			var is_start := not prev.has_geometry()
+			var is_end := is_penultimate(index)
+			var tri := 0
+			set_tri(cur.iTri, cur.iVert, tri, BRT, BMT, FMT)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BRT, FMT, FRT)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BRB, FRB, FMB)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BRB, FMB, BMB)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BMB, FLB, BLB)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BMB, FMB, FLB)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BMT, FLT, FMT)
+			tri += 1
+			set_tri(cur.iTri, cur.iVert, tri, BMT, BLT, FLT)
+
+			var up_normal := cur.nSurface
+			if is_start:
+				var start_size := pressured_size(prev.smoothedPressure)
+				var alpha := pressured_opacity(prev.smoothedPressure)
+				var right := cur.nRight * (start_size * 0.5)
+				set_vert(cur.iVert, BRT, prev.point.m_Pos + right, up_normal, m_Color, alpha)
+				set_vert(cur.iVert, BRB, prev.point.m_Pos + right, -up_normal, m_Color, alpha)
+				set_vert(cur.iVert, BMT, prev.point.m_Pos, up_normal, m_Color, alpha)
+				set_vert(cur.iVert, BMB, prev.point.m_Pos, -up_normal, m_Color, alpha)
+				set_vert(cur.iVert, BLT, prev.point.m_Pos - right, up_normal, m_Color, alpha)
+				set_vert(cur.iVert, BLB, prev.point.m_Pos - right, -up_normal, m_Color, alpha)
+
+			var size := pressured_size(cur.smoothedPressure)
+			var alpha := pressured_opacity(cur.smoothedPressure)
+			var right := cur.nRight * (size * 0.5)
+			var up := cur.nSurface * (belly * size * 0.5)
+			var cos_up: Vector3
+			var sin_right: Vector3
+			if is_end:
+				cos_up = up_normal
+				up = Vector3.ZERO
+				sin_right = Vector3.ZERO
+			else:
+				cos_up = cos_theta * cur.nSurface
+				sin_right = sin_theta * cur.nRight
+
+			set_vert(cur.iVert, FRT, cur.point.m_Pos + right, cos_up + sin_right, m_Color, alpha)
+			set_vert(cur.iVert, FRB, cur.point.m_Pos + right, -cos_up + sin_right, m_Color, alpha)
+			set_vert(cur.iVert, FMT, cur.point.m_Pos + up, up_normal, m_Color, alpha)
+			set_vert(cur.iVert, FMB, cur.point.m_Pos - up, -up_normal, m_Color, alpha)
+			set_vert(cur.iVert, FLT, cur.point.m_Pos - right, cos_up - sin_right, m_Color, alpha)
+			set_vert(cur.iVert, FLB, cur.point.m_Pos - right, -cos_up - sin_right, m_Color, alpha)
+		prev = cur
+
+func on_changed_stretch_uvs(changed_knot: int) -> void:
+	var knot0 := changed_knot
+	while m_knots[knot0 - 1].has_geometry():
+		knot0 -= 1
+
+	while knot0 < m_knots.size():
+		var total_length := 0.0
+		var knot1 := knot0
+		while knot1 < m_knots.size():
+			var cur := m_knots[knot1]
+			if not cur.has_geometry():
+				break
+			total_length += cur.length
+			knot1 += 1
+
+		var random01 := m_rng.in01(m_knots[knot0].iVert - 1)
+		var num_v: int = max(m_Desc.m_TextureAtlasV, 1)
+		var atlas := int(random01 * 3331.0) % num_v
+		var v0 := (atlas + m_TextureEdgeChop) / float(num_v)
+		var v1 := (atlas + 1.0 - m_TextureEdgeChop) / float(num_v)
+		var vmid := (v0 + v1) * 0.5
+		var distance := 0.0
+		for index in range(knot0, knot1):
+			var cur := m_knots[index]
+			distance += cur.length
+			var u := distance / total_length if total_length != 0.0 else 1.0
+			if index == knot0:
+				set_uv0(cur.iVert, BLT, Vector2(0.0, v0))
+				set_uv0(cur.iVert, BLB, Vector2(0.0, v0))
+				set_uv0(cur.iVert, BRT, Vector2(0.0, v1))
+				set_uv0(cur.iVert, BRB, Vector2(0.0, v1))
+				set_uv0(cur.iVert, BMT, Vector2(0.0, vmid))
+				set_uv0(cur.iVert, BMB, Vector2(0.0, vmid))
+			set_uv0(cur.iVert, FLT, Vector2(u, v0))
+			set_uv0(cur.iVert, FLB, Vector2(u, v0))
+			set_uv0(cur.iVert, FRT, Vector2(u, v1))
+			set_uv0(cur.iVert, FRB, Vector2(u, v1))
+			set_uv0(cur.iVert, FMT, Vector2(u, vmid))
+			set_uv0(cur.iVert, FMB, Vector2(u, vmid))
+
+		knot0 = knot1 + 1
+
+func on_changed_distance_uvs(knot_index: int) -> void:
+	var prev := m_knots[knot_index - 1]
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		if cur.has_geometry():
+			var u0: float
+			var v0: float
+			var v1: float
+			if prev.has_geometry():
+				var u0v0 := m_geometry.m_Texcoord0.v2[prev.iVert + FLT * NS]
+				u0 = u0v0.x
+				v0 = u0v0.y
+				v1 = m_geometry.m_Texcoord0.v2[prev.iVert + FRT * NS].y
+			else:
+				var random01 := m_rng.in01(cur.iVert - 1)
+				u0 = random01
+				var num_v: int = max(m_Desc.m_TextureAtlasV, 1)
+				var atlas := int(random01 * 3331.0) % num_v
+				v0 = (atlas + m_TextureEdgeChop) / float(num_v)
+				v1 = (atlas + 1.0 - m_TextureEdgeChop) / float(num_v)
+				var vmid := (v0 + v1) * 0.5
+				set_uv0(cur.iVert, BLT, Vector2(u0, v0))
+				set_uv0(cur.iVert, BLB, Vector2(u0, v0))
+				set_uv0(cur.iVert, BRT, Vector2(u0, v1))
+				set_uv0(cur.iVert, BRB, Vector2(u0, v1))
+				set_uv0(cur.iVert, BMT, Vector2(u0, vmid))
+				set_uv0(cur.iVert, BMB, Vector2(u0, vmid))
+
+			var length := m_knots[index].length
+			var size := pressured_size(m_knots[index].smoothedPressure)
+			var u1 := u0 + m_Desc.m_TileRate * (length / size)
+			var vmid := (v0 + v1) * 0.5
+			set_uv0(cur.iVert, FLT, Vector2(u1, v0))
+			set_uv0(cur.iVert, FLB, Vector2(u1, v0))
+			set_uv0(cur.iVert, FRT, Vector2(u1, v1))
+			set_uv0(cur.iVert, FRB, Vector2(u1, v1))
+			set_uv0(cur.iVert, FMT, Vector2(u1, vmid))
+			set_uv0(cur.iVert, FMB, Vector2(u1, vmid))
+		prev = cur
+
+func on_changed_tangents(knot_index: int) -> void:
+	var prev := m_knots[knot_index - 1]
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		if cur.has_geometry():
+			var st := BaseBrushScript.compute_st(m_geometry.m_Vertices, m_geometry.m_Texcoord0.v2, cur.iVert, BRT, BMT, FMT)
+			var s: Vector3 = st.s
+			if not prev.has_geometry():
+				set_tangent(cur.iVert, BLT, s)
+				set_tangent(cur.iVert, BLB, s)
+				set_tangent(cur.iVert, BRT, s)
+				set_tangent(cur.iVert, BRB, s)
+				set_tangent(cur.iVert, BMT, s)
+				set_tangent(cur.iVert, BMB, s)
+			set_tangent(cur.iVert, FLT, s)
+			set_tangent(cur.iVert, FLB, s)
+			set_tangent(cur.iVert, FRT, s)
+			set_tangent(cur.iVert, FRB, s)
+			set_tangent(cur.iVert, FMT, s)
+			set_tangent(cur.iVert, FMB, s)
+		prev = cur
+
+func is_penultimate(knot_index: int) -> bool:
+	return knot_index + 1 == m_knots.size() or not m_knots[knot_index + 1].has_geometry()
---
+++ Assets/Tests/GDScript/ThickGeometryBrushParityTest.gd
@@ -0,0 +1,97 @@
+extends SceneTree
+
+var _failures := 0
+
+func _init() -> void:
+	_run()
+	quit(1 if _failures > 0 else 0)
+
+func _run() -> void:
+	_check_distance_uv_geometry()
+	_check_stretch_uv_geometry()
+	if _failures == 0:
+		print("GDSCRIPT_PARITY_THICKBRUSH: all checks passed")
+
+func _check_distance_uv_geometry() -> void:
+	var brush := _make_thick_brush(ThickGeometryBrush.UVStyle.DISTANCE)
+	_expect(brush.update_position_ls(TrTransform.trs(Vector3.RIGHT, Quaternion.IDENTITY, 1.0), 1.0), "thick distance first update keeps")
+	brush.apply_changes_to_visuals()
+
+	_expect(brush.check_knot_invariants(), "thick distance invariants")
+	_expect_equal(brush.m_geometry.num_verts(), 12, "thick distance vertex count")
+	_expect_equal(brush.m_geometry.num_tri_indices(), 24, "thick distance tri index count")
+	_expect_equal(brush.m_geometry.m_Tris.slice(0, 6), [0, 2, 8, 0, 8, 6], "thick first top-right tris")
+	_expect_vec3_close(brush.m_geometry.m_Vertices[ThickGeometryBrush.BRT], Vector3(0.0, 0.5, 0.0), "thick back right top")
+	_expect_vec3_close(brush.m_geometry.m_Vertices[ThickGeometryBrush.BMT], Vector3.ZERO, "thick back middle top")
+	_expect_vec3_close(brush.m_geometry.m_Vertices[ThickGeometryBrush.BLT], Vector3(0.0, -0.5, 0.0), "thick back left top")
+	_expect_vec3_close(brush.m_geometry.m_Vertices[ThickGeometryBrush.FMT], Vector3.RIGHT, "thick front pinched middle")
+	_expect_vec3_close(brush.m_geometry.m_Normals[ThickGeometryBrush.BRT], Vector3.BACK, "thick back top normal")
+	_expect_vec3_close(brush.m_geometry.m_Normals[ThickGeometryBrush.BRB], -Vector3.BACK, "thick back bottom normal")
+	_expect_equal(brush.m_geometry.m_Colors[0], Color(0.2, 0.7, 0.4, 1.0), "thick color")
+	_expect_close(brush.m_geometry.m_Texcoord0.v2[ThickGeometryBrush.BLT].y, 0.1, "thick uv left chop")
+	_expect_close(brush.m_geometry.m_Texcoord0.v2[ThickGeometryBrush.BRT].y, 0.9, "thick uv right chop")
+	_expect_close(brush.m_geometry.m_Texcoord0.v2[ThickGeometryBrush.FLT].x - brush.m_geometry.m_Texcoord0.v2[ThickGeometryBrush.BLT].x, 1.0, "thick distance u delta")
+	_expect_close(brush.m_geometry.m_Tangents[0].length(), sqrt(2.0), "thick tangent length includes handedness")
+	brush.finalize_solitary_brush()
+	_expect_equal(brush.mesh_data.vertices.size(), 12, "thick finalized vertex count")
+	_expect(brush.m_geometry == null, "thick releases geometry")
+	brush.free()
+
+func _check_stretch_uv_geometry() -> void:
+	var brush := _make_thick_brush(ThickGeometryBrush.UVStyle.STRETCH)
+	_expect(brush.update_position_ls(TrTransform.trs(Vector3.RIGHT, Quaternion.IDENTITY, 1.0), 1.0), "thick stretch first update keeps")
+	_expect(brush.update_position_ls(TrTransform.trs(Vector3(2.0, 0.0, 0.0), Quaternion.IDENTITY, 1.0), 1.0), "thick stretch second update keeps")
+	brush.apply_changes_to_visuals()
+
+	_expect_equal(brush.m_geometry.num_verts(), 18, "thick stretch shared vertex count")
+	_expect_equal(brush.m_geometry.num_tri_indices(), 48, "thick stretch tri index count")
+	_expect_equal(brush.m_knots[1].iVert, 0, "thick first knot vertex start")
+	_expect_equal(brush.m_knots[2].iVert, 6, "thick second knot shared start")
+	_expect_equal(brush.m_geometry.m_Texcoord0.v2[ThickGeometryBrush.BLT].x, 0.0, "thick stretch start x")
+	_expect_close(brush.m_geometry.m_Texcoord0.v2[ThickGeometryBrush.FLT].x, 0.5, "thick stretch first end x")
+	_expect_close(brush.m_geometry.m_Texcoord0.v2[6 + ThickGeometryBrush.FLT].x, 1.0, "thick stretch second end x")
+	brush.finalize_solitary_brush()
+	brush.free()
+
+func _make_thick_brush(uv_style: int) -> ThickGeometryBrush:
+	var desc := BrushDescriptor.new()
+	desc.m_DurableName = "Thick"
+	desc.m_RenderBackfaces = false
+	desc.m_BackIsInvisible = false
+	desc.m_M11Compatibility = true
+	desc.m_TextureAtlasV = 1
+	desc.m_TileRate = 1.0
+	desc.m_PressureSizeRange = Vector2(1.0, 1.0)
+	desc.m_PressureOpacityRange = Vector2(1.0, 1.0)
+	desc.m_Opacity = 1.0
+	desc.m_SizeVariance = 0.0
+
+	var brush := ThickGeometryBrush.new()
+	brush.m_uvStyle = uv_style
+	brush.m_BaseSize_PS = 1.0
+	brush.m_Color = Color(0.2, 0.7, 0.4, 1.0)
+	brush.set_random_seed(0)
+	brush.init_brush(desc, TrTransform.identity())
+	brush.set_random_seed(0)
+	return brush
+
+func _expect(condition: bool, label: String) -> void:
+	if not condition:
+		_fail(label)
+
+func _expect_equal(actual: Variant, expected: Variant, label: String) -> void:
+	if actual != expected:
+		_fail("%s expected %s but got %s" % [label, expected, actual])
+
+func _expect_vec3_close(actual: Vector3, expected: Vector3, label: String) -> void:
+	_expect_close(actual.x, expected.x, "%s x" % label)
+	_expect_close(actual.y, expected.y, "%s y" % label)
+	_expect_close(actual.z, expected.z, "%s z" % label)
+
+func _expect_close(actual: float, expected: float, label: String) -> void:
+	if abs(actual - expected) > 1e-5:
+		_fail("%s expected %.8f but got %.8f" % [label, expected, actual])
+
+func _fail(message: String) -> void:
+	_failures += 1
+	push_error("GDSCRIPT_PARITY_THICKBRUSH: %s" % message)
