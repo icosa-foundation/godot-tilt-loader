da22091a16d8c7c11f4b17db2db760bc2206938b
Andy Baker <andy@andybak.net>
2026-06-17 15:24:51 +0100

    Port genius particles brush to GDScript

---
+++ Assets/Scripts/Brushes/GeniusParticlesBrush.gd
@@ -0,0 +1,264 @@
+class_name GeniusParticlesBrush
+extends GeometryBrush
+
+const K_SPAWN_INTERVAL_PS := 0.0025 * App.METERS_TO_UNITS
+const K_SINGLE_PARTICLE_TRIGGER_PRESSURE := 0.8
+const K_VERTS_IN_SOLID := 4
+const K_TRIS_IN_SOLID := 2
+
+const K_SALT_MAX_PARTICLES_PER_KNOT := 16
+const K_SALT_MAX_SALTS_PER_PARTICLE := 16
+const K_SALT_PRESSURE := 0
+const K_SALT_ALPHA := K_SALT_PRESSURE + 1
+const K_SALT_ON_SPHERE := K_SALT_ALPHA + 1
+const K_SALT_ROTATION := K_SALT_ON_SPHERE + 2
+const K_SALT_ROLL := K_SALT_ROTATION + 3
+const K_SALT_ATLAS := K_SALT_ROLL + 1
+
+const BR := 0
+const BL := 1
+const FR := 2
+const FL := 3
+
+const TEXTURE_ATLAS_00 := Vector4(0.0, 0.0, 0.0, 0.0)
+const TEXTURE_ATLAS_05 := Vector4(0.0, 0.5, 0.0, 0.0)
+const TEXTURE_ATLAS_50 := Vector4(0.5, 0.0, 0.0, 0.0)
+const TEXTURE_ATLAS_55 := Vector4(0.5, 0.5, 0.0, 0.0)
+
+var m_DecayTimers: Array[float] = []
+var m_DecayedKnots := 0
+var m_DistancePointerTravelled := -1.0
+var m_LastPos := Vector3.ZERO
+var m_LengthsAtKnot: Array[float] = [0.0]
+var m_SpawnInterval := 0.0
+var m_ParticleSizeScale := 1.0
+
+func _init() -> void:
+	setup_geometry_brush(true, K_VERTS_IN_SOLID, false, false)
+
+func calculate_salt(knot_index: int, particle_index: int) -> int:
+	var pretend_knot_index := knot_index + m_DecayedKnots
+	return K_SALT_MAX_SALTS_PER_PARTICLE * (pretend_knot_index * K_SALT_MAX_PARTICLES_PER_KNOT + particle_index)
+
+func get_spawn_interval(_pressure01: float) -> float:
+	return m_SpawnInterval
+
+func distance_from_knot(knot_index: int, position: Vector3) -> float:
+	var distance := super.distance_from_knot(knot_index, position)
+	if knot_index == m_knots.size() - 2:
+		distance = m_DistancePointerTravelled
+	return distance
+
+func always_rebuild_preview_brush() -> bool:
+	return false
+
+func init_brush(desc: BrushDescriptor, local_pointer_xf: TrTransform) -> void:
+	super.init_brush(desc, local_pointer_xf)
+	m_DecayTimers.clear()
+	m_geometry.set_layout(get_vertex_layout(desc))
+	m_SpawnInterval = (K_SPAWN_INTERVAL_PS * pointer_to_local()) / maxf(m_Desc.m_ParticleRate, 0.000001)
+	m_ParticleSizeScale = m_Desc.m_ParticleSpeed / maxf(m_Desc.m_BrushSizeRange.x, 0.000001)
+	m_LengthsAtKnot = [0.0]
+	m_DecayedKnots = 0
+	m_DistancePointerTravelled = -1.0
+
+func get_vertex_layout(_desc: BrushDescriptor) -> GeometryPool.VertexLayout:
+	var layout := GeometryPool.VertexLayout.create(
+		GeometryPool.TexcoordInfo.create(4, GeometryPool.Semantic.XY_IS_UV),
+		GeometryPool.TexcoordInfo.create(3, GeometryPool.Semantic.POSITION),
+		null,
+		true,
+		true,
+		false
+	)
+	layout.normalSemantic = GeometryPool.Semantic.POSITION
+	layout.bUseVertexIds = true
+	layout.bFbxExportNormalAsTexcoord1 = true
+	return layout
+
+func decay_brush() -> void:
+	var knots_to_shift := 0
+	for index in range(m_DecayTimers.size()):
+		m_DecayTimers[index] += 0.0
+		if m_DecayTimers[index] > K_PREVIEW_DURATION:
+			knots_to_shift += 1
+	if knots_to_shift <= 0:
+		return
+	m_DecayTimers = m_DecayTimers.slice(knots_to_shift)
+	remove_initial_knots(knots_to_shift)
+	if knots_to_shift > 0 and knots_to_shift < m_LengthsAtKnot.size():
+		var length_reduction := floorf(m_LengthsAtKnot[knots_to_shift] / m_SpawnInterval) * m_SpawnInterval
+		var new_count := m_LengthsAtKnot.size() - knots_to_shift
+		for index in range(1, new_count):
+			m_LengthsAtKnot[index] = m_LengthsAtKnot[index + knots_to_shift] - length_reduction
+		ListUtils.set_count(m_LengthsAtKnot, new_count, 0.0)
+	m_DecayedKnots += knots_to_shift
+
+func update_position_impl(position: Vector3, orientation: Quaternion, pressure: float) -> bool:
+	var result := super.update_position_impl(position, orientation, pressure)
+	if m_DistancePointerTravelled < 0.0:
+		m_DistancePointerTravelled = 0.0
+	else:
+		m_DistancePointerTravelled += (position - m_LastPos).length()
+	m_LastPos = position
+	if m_PreviewMode and result:
+		m_DecayTimers.append(0.0)
+	return result
+
+func reset_brush_for_preview(local_pointer_xf: TrTransform) -> void:
+	super.reset_brush_for_preview(local_pointer_xf)
+	m_DecayTimers.clear()
+
+func control_points_changed(first_knot_index: int) -> void:
+	var num_knots := m_knots.size()
+	var previous_index := 0 if first_knot_index == 0 else first_knot_index - 1
+	invalidate_lengths_from_knot(first_knot_index)
+
+	var particles_at_prev := total_particles_at_knot(previous_index)
+	for knot_index in range(first_knot_index, num_knots):
+		var particles_at_cur := total_particles_at_knot(knot_index)
+		var particles_for_knot := particles_at_cur - particles_at_prev
+		if knot_index == num_knots - 1:
+			particles_for_knot += 1
+		set_geometry_space_for_knot(knot_index, particles_for_knot)
+		particles_at_prev = particles_at_cur
+
+	resize_geometry()
+
+	var prev := m_knots[previous_index]
+	particles_at_prev = total_particles_at_knot(previous_index)
+	var prev_length := stroke_length_at_knot(previous_index)
+	for knot_index in range(first_knot_index, num_knots):
+		var cur := m_knots[knot_index]
+		var particles_for_knot := int(cur.nTri / K_TRIS_IN_SOLID)
+		var cur_length := stroke_length_at_knot(knot_index)
+		for particle_index in range(particles_for_knot):
+			var particle_dist_on_stroke := (particles_at_prev + particle_index) * m_SpawnInterval
+			var lerp_ratio := _inverse_lerp(prev_length, cur_length, particle_dist_on_stroke)
+			var particle_pos := prev.point.m_Pos.lerp(cur.point.m_Pos, lerp_ratio)
+			if knot_index == num_knots - 1 and particle_index == particles_for_knot - 1:
+				particle_pos = cur.point.m_Pos
+			var salt := calculate_salt(knot_index, particle_index)
+			var size := pressured_random_size(cur.smoothedPressure, salt + K_SALT_PRESSURE)
+			create_particle_geometry(knot_index, particle_index, particle_pos, size)
+		particles_at_prev = int(floorf(cur_length / m_SpawnInterval)) + 1
+		prev_length = cur_length
+		prev = cur
+
+func needs_straight_edge_proxy() -> bool:
+	return true
+
+func finalize_solitary_brush() -> void:
+	finalize_particle_mesh()
+	super.finalize_solitary_brush()
+
+func stroke_length_at_knot(knot_index: int) -> float:
+	if knot_index < m_LengthsAtKnot.size():
+		return m_LengthsAtKnot[knot_index]
+	var num_lengths := m_LengthsAtKnot.size()
+	ListUtils.set_count(m_LengthsAtKnot, knot_index + 1, 0.0)
+	var prev := m_knots[num_lengths - 1]
+	var length := m_LengthsAtKnot[num_lengths - 1]
+	for index in range(num_lengths, knot_index + 1):
+		var cur := m_knots[index]
+		length += (cur.point.m_Pos - prev.point.m_Pos).length()
+		m_LengthsAtKnot[index] = length
+		prev = cur
+	return length
+
+func invalidate_lengths_from_knot(knot_index: int) -> void:
+	ListUtils.set_count(m_LengthsAtKnot, maxi(knot_index, 1), 0.0)
+
+func total_particles_at_knot(knot_index: int) -> int:
+	if knot_index == 0:
+		return 0
+	return int(floorf(stroke_length_at_knot(knot_index) / m_SpawnInterval)) + 1
+
+func finalize_particle_mesh() -> void:
+	if m_knots.size() < 2:
+		return
+	var final := m_knots[m_knots.size() - 1]
+	if final.nTri <= K_TRIS_IN_SOLID * NS:
+		m_knots = m_knots.slice(0, m_knots.size() - 1)
+	else:
+		final.nTri -= K_TRIS_IN_SOLID * NS
+		final.nVert -= K_VERTS_IN_SOLID * NS
+		m_knots[m_knots.size() - 1] = final
+	resize_geometry()
+
+	if m_knots.size() == 2:
+		final = m_knots[1]
+		var last_particle := int(final.nTri / K_TRIS_IN_SOLID) - 1
+		if last_particle >= 0:
+			var pressure: float = maxf(K_SINGLE_PARTICLE_TRIGGER_PRESSURE, final.smoothedPressure)
+			var salt := calculate_salt(1, last_particle)
+			var size := pressured_random_size(pressure, salt)
+			create_particle_geometry(1, last_particle, m_knots[0].point.m_Pos, size)
+
+func set_geometry_space_for_knot(knot_index: int, particle_count: int) -> void:
+	var cur := m_knots[knot_index]
+	var prev := m_knots[knot_index - 1] if knot_index > 0 else Knot.new()
+	cur.iTri = prev.iTri + prev.nTri
+	cur.iVert = prev.iVert + prev.nVert
+	cur.length = (cur.point.m_Pos - prev.point.m_Pos).length()
+	cur.nRight = Vector3.RIGHT
+	cur.nTri = K_TRIS_IN_SOLID * particle_count
+	cur.nVert = K_VERTS_IN_SOLID * particle_count
+	m_knots[knot_index] = cur
+
+func create_particle_geometry(knot_index: int, particle_index: int, position: Vector3, size: float) -> void:
+	var cur := m_knots[knot_index]
+	var vert_index := cur.iVert + particle_index * K_VERTS_IN_SOLID * NS
+	var tri_index := cur.iTri + particle_index * K_TRIS_IN_SOLID * NS
+	var salt := calculate_salt(knot_index, particle_index)
+	var alpha: float
+	if m_Desc.m_RandomizeAlpha:
+		alpha = m_rng.in01(salt + K_SALT_ALPHA)
+	else:
+		alpha = m_Desc.m_Opacity * lerpf(m_Desc.m_PressureOpacityRange.x, m_Desc.m_PressureOpacityRange.y, cur.smoothedPressure)
+	var random_offset := m_rng.on_unit_sphere(salt + K_SALT_ON_SPHERE) * size * m_ParticleSizeScale
+	var center := position + random_offset
+	var random_direction := m_rng.rotation(salt + K_SALT_ROTATION)
+	var up_offset := random_direction * (Vector3.UP * size * 0.5)
+	var right_offset := random_direction * (Vector3.RIGHT * size * 0.5)
+
+	set_tri(tri_index, vert_index, 0, BR, BL, FL)
+	set_tri(tri_index, vert_index, 1, BR, FL, FR)
+	set_vert(vert_index, BR, center - up_offset + right_offset, center, m_Color, alpha)
+	set_vert(vert_index, BL, center - up_offset - right_offset, center, m_Color, alpha)
+	set_vert(vert_index, FR, center + up_offset + right_offset, center, m_Color, alpha)
+	set_vert(vert_index, FL, center + up_offset - right_offset, center, m_Color, alpha)
+
+	var time := 0.0
+	var half_rotate_range := m_Desc.m_ParticleInitialRotationRange * 0.5
+	var rotation := deg_to_rad(m_rng.in_range(salt + K_SALT_ROLL, -half_rotate_range, half_rotate_range))
+	var uv0 := Vector4(0.0, 0.0, rotation, time)
+	var uv1 := position
+
+	if m_Desc.m_TextureAtlasV > 1:
+		var rand := m_rng.in_int_range(salt + K_SALT_ATLAS, 0, 4)
+		var offset := TEXTURE_ATLAS_00
+		if rand == 1:
+			offset = TEXTURE_ATLAS_50
+		elif rand == 2:
+			offset = TEXTURE_ATLAS_05
+		elif rand == 3:
+			offset = TEXTURE_ATLAS_55
+		set_uv0(vert_index, BL, TEXTURE_ATLAS_00 + offset + uv0)
+		set_uv0(vert_index, FL, TEXTURE_ATLAS_50 + offset + uv0)
+		set_uv0(vert_index, BR, TEXTURE_ATLAS_05 + offset + uv0)
+		set_uv0(vert_index, FR, TEXTURE_ATLAS_55 + offset + uv0)
+	else:
+		set_uv0(vert_index, BL, uv0 + (TEXTURE_ATLAS_00 * 2.0))
+		set_uv0(vert_index, FL, uv0 + (TEXTURE_ATLAS_50 * 2.0))
+		set_uv0(vert_index, BR, uv0 + (TEXTURE_ATLAS_05 * 2.0))
+		set_uv0(vert_index, FR, uv0 + (TEXTURE_ATLAS_55 * 2.0))
+	set_uv1(vert_index, BL, uv1)
+	set_uv1(vert_index, FL, uv1)
+	set_uv1(vert_index, BR, uv1)
+	set_uv1(vert_index, FR, uv1)
+
+static func _inverse_lerp(a: float, b: float, value: float) -> float:
+	if absf(b - a) < 0.000001:
+		return 0.0
+	return clampf((value - a) / (b - a), 0.0, 1.0)
---
+++ Assets/Tests/GDScript/GeniusParticlesBrushParityTest.gd
@@ -0,0 +1,98 @@
+extends SceneTree
+
+var _failures := 0
+
+func _init() -> void:
+	_run()
+	quit(1 if _failures > 0 else 0)
+
+func _run() -> void:
+	_check_particle_geometry()
+	_check_finalize_removes_hanging_particle()
+	if _failures == 0:
+		print("GDSCRIPT_PARITY_GENIUSPARTICLES: all checks passed")
+
+func _check_particle_geometry() -> void:
+	var brush := _make_genius_brush()
+	_expect(not brush.update_position_ls(TrTransform.trs(Vector3.RIGHT, Quaternion.IDENTITY, 1.0), 1.0), "genius first update waits for travelled distance")
+	brush.apply_changes_to_visuals()
+
+	_expect(brush.check_knot_invariants(), "genius knot invariants")
+	_expect_equal(brush.m_geometry.get_layout().texcoord0.size, 4, "genius uv0 size")
+	_expect_equal(brush.m_geometry.get_layout().texcoord1.size, 3, "genius uv1 size")
+	_expect(not brush.m_geometry.get_layout().bUseTangents, "genius layout omits tangents")
+	_expect_equal(brush.m_geometry.num_verts(), 24, "genius vertex count with hanging particle")
+	_expect_equal(brush.m_geometry.num_tri_indices(), 36, "genius tri count with hanging particle")
+	_expect_equal(brush.m_knots[1].nVert, 24, "genius active knot verts include hanging particle")
+	_expect_equal(brush.m_geometry.m_Tris.slice(0, 6), [0, 1, 3, 0, 3, 2], "genius first particle tris")
+	_expect_equal(brush.m_geometry.m_Texcoord0.v4.size(), 24, "genius uv0 count")
+	_expect_equal(brush.m_geometry.m_Texcoord1.v3.size(), 24, "genius uv1 count")
+	_expect_equal(brush.m_geometry.m_Tangents.size(), 0, "genius tangent count")
+	_expect_vec3_close(brush.m_geometry.m_Normals[0], brush.m_geometry.m_Texcoord1.v3[0], "genius normal stores center")
+	_expect_vec3_close(brush.m_geometry.m_Texcoord1.v3[0], Vector3.ZERO, "genius first particle position")
+	_expect_vec3_close(brush.m_geometry.m_Texcoord1.v3[20], Vector3.RIGHT, "genius hanging particle position")
+	_expect_close(brush.m_geometry.m_Texcoord0.v4[1].x, 0.0, "genius uv bl x")
+	_expect_close(brush.m_geometry.m_Texcoord0.v4[3].x, 1.0, "genius uv fl x")
+	_expect_close(brush.m_geometry.m_Texcoord0.v4[0].y, 1.0, "genius uv br y")
+	brush.free()
+
+func _check_finalize_removes_hanging_particle() -> void:
+	var brush := _make_genius_brush()
+	_expect(not brush.update_position_ls(TrTransform.trs(Vector3.RIGHT, Quaternion.IDENTITY, 1.0), 1.0), "genius finalize update waits for travelled distance")
+	brush.apply_changes_to_visuals()
+	brush.finalize_solitary_brush()
+
+	_expect(brush.m_geometry == null, "genius releases geometry")
+	_expect_equal(brush.mesh_data.vertices.size(), 20, "genius finalized vertex count")
+	_expect_equal(brush.mesh_data.triangles.size(), 30, "genius finalized tri count")
+	_expect_equal(brush.mesh_data.uv0_v4.size(), 20, "genius finalized uv0 count")
+	_expect_equal(brush.mesh_data.uv1_v3.size(), 20, "genius finalized uv1 count")
+	_expect_vec3_close(brush.mesh_data.uv1_v3[16], Vector3.ZERO, "genius single-stroke final particle reset to initial position")
+	brush.free()
+
+func _make_genius_brush() -> GeniusParticlesBrush:
+	var desc := BrushDescriptor.new()
+	desc.m_DurableName = "GeniusParticles"
+	desc.m_RenderBackfaces = false
+	desc.m_BackIsInvisible = false
+	desc.m_M11Compatibility = false
+	desc.m_TextureAtlasV = 1
+	desc.m_TileRate = 1.0
+	desc.m_PressureSizeRange = Vector2(1.0, 1.0)
+	desc.m_PressureOpacityRange = Vector2(1.0, 1.0)
+	desc.m_Opacity = 1.0
+	desc.m_SizeVariance = 0.0
+	desc.m_ParticleRate = 0.1
+	desc.m_ParticleSpeed = 0.0
+	desc.m_ParticleInitialRotationRange = 0.0
+	desc.m_RandomizeAlpha = false
+	desc.m_BrushSizeRange = Vector2(1.0, 2.0)
+
+	var brush := GeniusParticlesBrush.new()
+	brush.m_BaseSize_PS = 1.0
+	brush.m_Color = Color(0.9, 0.7, 0.2, 1.0)
+	brush.set_random_seed(0)
+	brush.init_brush(desc, TrTransform.identity())
+	brush.set_random_seed(0)
+	return brush
+
+func _expect(condition: bool, label: String) -> void:
+	if not condition:
+		_fail(label)
+
+func _expect_equal(actual: Variant, expected: Variant, label: String) -> void:
+	if actual != expected:
+		_fail("%s expected %s but got %s" % [label, expected, actual])
+
+func _expect_vec3_close(actual: Vector3, expected: Vector3, label: String) -> void:
+	_expect_close(actual.x, expected.x, "%s x" % label)
+	_expect_close(actual.y, expected.y, "%s y" % label)
+	_expect_close(actual.z, expected.z, "%s z" % label)
+
+func _expect_close(actual: float, expected: float, label: String) -> void:
+	if absf(actual - expected) > 1e-5:
+		_fail("%s expected %.8f but got %.8f" % [label, expected, actual])
+
+func _fail(message: String) -> void:
+	_failures += 1
+	push_error("GDSCRIPT_PARITY_GENIUSPARTICLES: %s" % message)
