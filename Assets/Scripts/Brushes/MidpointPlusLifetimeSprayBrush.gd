5e0dbe2aac386c07ba363630eb7862da3dcd29fc
Andy Baker <andy@andybak.net>
2026-06-17 15:09:14 +0100

    Port midpoint spray brush to GDScript

---
+++ Assets/Scripts/Brushes/MidpointPlusLifetimeSprayBrush.gd
@@ -0,0 +1,213 @@
+class_name MidpointPlusLifetimeSprayBrush
+extends GeometryBrush
+
+const K_VERTS_IN_SOLID := 4
+const K_TRIS_IN_SOLID := 2
+const K_MAX_QUADS_PER_KNOT := 500
+
+const K_SALT_MAX_QUADS_PER_KNOT := 5
+const K_SALT_MAX_SALTS_PER_QUAD := 10
+const K_SALT_PRESSURE := 0
+const K_SALT_ROTATION := K_SALT_PRESSURE + 1
+const K_SALT_POSITION := K_SALT_ROTATION + 1
+const K_SALT_ALPHA := K_SALT_POSITION + 3
+const K_SALT_ATLAS := K_SALT_ALPHA + 1
+
+const BR := 0
+const BL := 1
+const FR := 2
+const FL := 3
+
+const TEXTURE_ATLAS_00 := Vector2(0.0, 0.0)
+const TEXTURE_ATLAS_05 := Vector2(0.0, 0.5)
+const TEXTURE_ATLAS_50 := Vector2(0.5, 0.0)
+const TEXTURE_ATLAS_55 := Vector2(0.5, 0.5)
+
+func _init() -> void:
+	setup_geometry_brush(true, K_VERTS_IN_SOLID, false)
+
+func init_brush(desc: BrushDescriptor, local_pointer_xf: TrTransform) -> void:
+	super.init_brush(desc, local_pointer_xf)
+	m_geometry.set_layout(get_vertex_layout(desc))
+
+func get_vertex_layout(_desc: BrushDescriptor) -> GeometryPool.VertexLayout:
+	return GeometryPool.VertexLayout.create(
+		GeometryPool.TexcoordInfo.create(2, GeometryPool.Semantic.XY_IS_UV),
+		GeometryPool.TexcoordInfo.create(4, GeometryPool.Semantic.VECTOR),
+		null,
+		true,
+		true,
+		true
+	)
+
+func get_spawn_interval(pressure01: float) -> float:
+	return pressured_size(pressure01) / m_Desc.m_SprayRateMultiplier
+
+func control_points_changed(knot_index: int) -> void:
+	on_changed_frame_knots(knot_index)
+	resize_geometry()
+	on_changed_make_geometry(knot_index)
+	on_changed_uvs(knot_index)
+	on_changed_tangents(knot_index)
+
+func on_changed_frame_knots(knot_index: int) -> void:
+	var prev := m_knots[knot_index - 1]
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		cur.iTri = prev.iTri + prev.nTri
+		cur.iVert = prev.iVert + prev.nVert
+		var move := cur.point.m_Pos - prev.point.m_Pos
+		cur.length = move.length()
+		var min_distance_to_spawn := get_spawn_interval(cur.smoothedPressure)
+		if cur.length < min_distance_to_spawn:
+			cur.nTri = 0
+			cur.nVert = 0
+			cur.nRight = Vector3.ZERO
+			cur.nSurface = Vector3.ZERO
+		else:
+			var facing := move.normalized()
+			var frame := BaseBrushScript.compute_surface_frame_new(Vector3.ZERO, facing, cur.point.m_Orient)
+			cur.nRight = frame.right
+			cur.nSurface = frame.normal
+			var num_quads: int = min(int(cur.length / min_distance_to_spawn), get_num_quads_allowed())
+			cur.nTri = num_quads * K_TRIS_IN_SOLID * NS
+			cur.nVert = num_quads * K_VERTS_IN_SOLID * NS
+		m_knots[index] = cur
+		prev = cur
+
+func on_changed_make_geometry(knot_index: int) -> void:
+	var prev := m_knots[knot_index - 1]
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		if cur.has_geometry():
+			var num_quads := int(cur.nTri / (K_TRIS_IN_SOLID * NS))
+			var move_direction := (cur.point.m_Pos - prev.point.m_Pos).normalized()
+			var min_distance_to_spawn := get_spawn_interval(cur.smoothedPressure)
+			var last_spawn_pos := prev.point.m_Pos
+			var vert_index := cur.iVert
+			var tri_index := cur.iTri
+			var alpha := pressured_opacity(cur.smoothedPressure)
+
+			for quad in range(num_quads):
+				var salt := K_SALT_MAX_SALTS_PER_QUAD * (index * K_SALT_MAX_QUADS_PER_KNOT + quad)
+				var center := last_spawn_pos
+				var right := cur.nRight
+				var facing := move_direction
+				var rotation_variance := m_Desc.m_RotationVariance
+				if rotation_variance > 0.0001:
+					var rotate := Quaternion(cur.nSurface.normalized(), deg_to_rad(m_rng.in_range(salt + K_SALT_ROTATION, -rotation_variance, rotation_variance)))
+					right = rotate * right
+					facing = rotate * facing
+
+				var size := pressured_random_size(cur.smoothedPressure, salt + K_SALT_PRESSURE)
+				var forward_offset := facing * (size * m_Desc.m_SizeRatio.x * 0.5)
+				var right_offset := right * (size * m_Desc.m_SizeRatio.y * 0.5)
+				center += size * m_Desc.m_PositionVariance * m_rng.in_unit_sphere(salt + K_SALT_POSITION)
+
+				set_tri(tri_index, vert_index, 0, BR, BL, FL)
+				set_tri(tri_index, vert_index, 1, BR, FL, FR)
+				if m_Desc.m_RandomizeAlpha:
+					alpha = m_rng.in_range(salt + K_SALT_ALPHA, 0.0, 1.0)
+
+				var creation_time := 0.0
+				set_vert(vert_index, BR, center - forward_offset + right_offset, cur.nSurface, m_Color, alpha)
+				set_uv1(vert_index, BR, _offset4(-forward_offset + right_offset, creation_time))
+				set_vert(vert_index, BL, center - forward_offset - right_offset, cur.nSurface, m_Color, alpha)
+				set_uv1(vert_index, BL, _offset4(-forward_offset - right_offset, creation_time))
+				set_vert(vert_index, FR, center + forward_offset + right_offset, cur.nSurface, m_Color, alpha)
+				set_uv1(vert_index, FR, _offset4(forward_offset + right_offset, creation_time))
+				set_vert(vert_index, FL, center + forward_offset - right_offset, cur.nSurface, m_Color, alpha)
+				set_uv1(vert_index, FL, _offset4(forward_offset - right_offset, creation_time))
+
+				tri_index += K_TRIS_IN_SOLID * NS
+				vert_index += K_VERTS_IN_SOLID * NS
+				last_spawn_pos += move_direction * min_distance_to_spawn
+		prev = cur
+
+func on_changed_uvs(knot_index: int) -> void:
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		if cur.has_geometry():
+			var quad := 0
+			for vert_index in range(cur.iVert, cur.iVert + cur.nVert, K_VERTS_IN_SOLID * NS):
+				if m_Desc.m_TextureAtlasV > 1:
+					var salt := K_SALT_MAX_SALTS_PER_QUAD * (index * K_SALT_MAX_QUADS_PER_KNOT + quad)
+					var rand := m_rng.in_int_range(salt + K_SALT_ATLAS, 0, 4)
+					var offset := Vector2.ZERO
+					if rand == 1:
+						offset = TEXTURE_ATLAS_50
+					elif rand == 2:
+						offset = TEXTURE_ATLAS_05
+					elif rand == 3:
+						offset = TEXTURE_ATLAS_55
+					set_uv0(vert_index, BL, TEXTURE_ATLAS_00 + offset)
+					set_uv0(vert_index, FL, TEXTURE_ATLAS_50 + offset)
+					set_uv0(vert_index, BR, TEXTURE_ATLAS_05 + offset)
+					set_uv0(vert_index, FR, TEXTURE_ATLAS_55 + offset)
+				else:
+					set_uv0(vert_index, BL, Vector2(0.0, 0.0))
+					set_uv0(vert_index, FL, Vector2(1.0, 0.0))
+					set_uv0(vert_index, BR, Vector2(0.0, 1.0))
+					set_uv0(vert_index, FR, Vector2(1.0, 1.0))
+				quad += 1
+
+func on_changed_tangents(knot_index: int) -> void:
+	for index in range(knot_index, m_knots.size()):
+		var cur := m_knots[index]
+		if cur.has_geometry():
+			for vert_index in range(cur.iVert, cur.iVert + cur.nVert, K_VERTS_IN_SOLID * NS):
+				var bl := m_geometry.m_Vertices[vert_index + BL * NS]
+				var fl := m_geometry.m_Vertices[vert_index + FL * NS]
+				var facing := fl - bl
+				set_tangent(vert_index, BL, facing)
+				set_tangent(vert_index, BR, facing)
+				set_tangent(vert_index, FL, facing)
+				set_tangent(vert_index, FR, facing)
+
+func finalize_solitary_brush() -> void:
+	finalize_particle_mesh()
+	super.finalize_solitary_brush()
+
+func finalize_particle_mesh() -> void:
+	if m_knots.size() < 2:
+		return
+	var final := m_knots[m_knots.size() - 1]
+	if final.nTri <= K_TRIS_IN_SOLID * NS:
+		m_knots = m_knots.slice(0, m_knots.size() - 1)
+	else:
+		final.nTri -= K_TRIS_IN_SOLID * NS
+		final.nVert -= K_VERTS_IN_SOLID * NS
+		m_knots[m_knots.size() - 1] = final
+	resize_geometry()
+	if m_knots.size() == 2:
+		final = m_knots[1]
+		var last_particle := int(final.nTri / K_TRIS_IN_SOLID) - 1
+		if last_particle >= 0:
+			var pressure: float = max(0.8, final.smoothedPressure)
+			var salt: int = K_SALT_MAX_SALTS_PER_QUAD * (1 * K_SALT_MAX_QUADS_PER_KNOT + last_particle)
+			var size := pressured_random_size(pressure, salt)
+			create_particle_geometry(1, last_particle, m_knots[0].point.m_Pos, size)
+
+func create_particle_geometry(knot_index: int, particle_index: int, position: Vector3, size: float) -> void:
+	var cur := m_knots[knot_index]
+	var vert_index := cur.iVert + particle_index * K_VERTS_IN_SOLID * NS
+	var tri_index := cur.iTri + particle_index * K_TRIS_IN_SOLID * NS
+	var salt := K_SALT_MAX_SALTS_PER_QUAD * (knot_index * K_SALT_MAX_QUADS_PER_KNOT + particle_index)
+	var alpha: float = m_rng.in01(salt + K_SALT_ALPHA) if m_Desc.m_RandomizeAlpha else pressured_opacity(cur.smoothedPressure)
+	var center := position
+	var up_offset := Vector3.UP * size * 0.5
+	var right_offset := Vector3.RIGHT * size * 0.5
+	set_tri(tri_index, vert_index, 0, BR, BL, FL)
+	set_tri(tri_index, vert_index, 1, BR, FL, FR)
+	set_vert(vert_index, BR, center - up_offset + right_offset, cur.nSurface, m_Color, alpha)
+	set_vert(vert_index, BL, center - up_offset - right_offset, cur.nSurface, m_Color, alpha)
+	set_vert(vert_index, FR, center + up_offset + right_offset, cur.nSurface, m_Color, alpha)
+	set_vert(vert_index, FL, center + up_offset - right_offset, cur.nSurface, m_Color, alpha)
+	on_changed_uvs(knot_index)
+
+func get_num_quads_allowed() -> int:
+	var max_num_verts := 0xffff
+	return min(int((max_num_verts - get_num_used_verts()) / (K_VERTS_IN_SOLID * NS)), K_MAX_QUADS_PER_KNOT)
+
+func _offset4(offset: Vector3, time: float) -> Vector4:
+	return Vector4(offset.x, offset.y, offset.z, time)
---
+++ Assets/Tests/GDScript/MidpointSprayBrushParityTest.gd
@@ -0,0 +1,92 @@
+extends SceneTree
+
+var _failures := 0
+
+func _init() -> void:
+	_run()
+	quit(1 if _failures > 0 else 0)
+
+func _run() -> void:
+	_check_midpoint_geometry_and_uv1()
+	_check_finalize_removes_hanging_particle()
+	if _failures == 0:
+		print("GDSCRIPT_PARITY_MIDPOINTSPRAY: all checks passed")
+
+func _check_midpoint_geometry_and_uv1() -> void:
+	var brush := _make_midpoint_brush()
+	_expect(brush.update_position_ls(TrTransform.trs(Vector3(2.0, 0.0, 0.0), Quaternion.IDENTITY, 1.0), 1.0), "midpoint update keeps")
+	brush.apply_changes_to_visuals()
+
+	var layout := brush.get_vertex_layout(brush.m_Desc)
+	_expect_equal(layout.texcoord1.size, 4, "midpoint uv1 size")
+	_expect_equal(layout.texcoord1.semantic, GeometryPool.Semantic.VECTOR, "midpoint uv1 semantic")
+	_expect_equal(brush.NS, 1, "midpoint single-sided stride")
+	_expect_equal(brush.m_geometry.num_verts(), 8, "midpoint vertex count")
+	_expect_equal(brush.m_geometry.num_tri_indices(), 12, "midpoint tri count")
+	_expect_equal(brush.m_geometry.m_Tris.slice(0, 6), [0, 1, 3, 0, 3, 2], "midpoint first tris")
+
+	_expect_vec3_close(brush.m_geometry.m_Vertices[MidpointPlusLifetimeSprayBrush.BR], Vector3(-0.5, 0.5, 0.0), "midpoint first br")
+	_expect_vec3_close(brush.m_geometry.m_Vertices[MidpointPlusLifetimeSprayBrush.FL], Vector3(0.5, -0.5, 0.0), "midpoint first fl")
+	_expect_equal(brush.m_geometry.m_Texcoord0.v2[MidpointPlusLifetimeSprayBrush.BL], Vector2(0.0, 0.0), "midpoint uv0 bl")
+	_expect_equal(brush.m_geometry.m_Texcoord0.v2[MidpointPlusLifetimeSprayBrush.FR], Vector2(1.0, 1.0), "midpoint uv0 fr")
+	_expect_equal(brush.m_geometry.m_Texcoord1.v4[MidpointPlusLifetimeSprayBrush.BR], Vector4(-0.5, 0.5, 0.0, 0.0), "midpoint uv1 br")
+	_expect_equal(brush.m_geometry.m_Texcoord1.v4[MidpointPlusLifetimeSprayBrush.FL], Vector4(0.5, -0.5, 0.0, 0.0), "midpoint uv1 fl")
+	_expect_close(brush.m_geometry.m_Tangents[0].length(), sqrt(2.0), "midpoint tangent length includes handedness")
+	_expect_equal(brush.m_geometry.m_Colors[0], Color(0.4, 0.8, 0.2, 1.0), "midpoint color")
+	brush.free()
+
+func _check_finalize_removes_hanging_particle() -> void:
+	var brush := _make_midpoint_brush()
+	_expect(brush.update_position_ls(TrTransform.trs(Vector3(2.0, 0.0, 0.0), Quaternion.IDENTITY, 1.0), 1.0), "midpoint finalize update keeps")
+	brush.apply_changes_to_visuals()
+	brush.finalize_solitary_brush()
+	_expect_equal(brush.mesh_data.vertices.size(), 8, "midpoint finalize keeps generated particles")
+	_expect_equal(brush.mesh_data.triangles.size(), 12, "midpoint finalize tri count")
+	_expect(brush.m_geometry == null, "midpoint releases geometry")
+	brush.free()
+
+func _make_midpoint_brush() -> MidpointPlusLifetimeSprayBrush:
+	var desc := BrushDescriptor.new()
+	desc.m_DurableName = "MidpointSpray"
+	desc.m_RenderBackfaces = false
+	desc.m_BackIsInvisible = false
+	desc.m_M11Compatibility = false
+	desc.m_TextureAtlasV = 1
+	desc.m_SprayRateMultiplier = 1.0
+	desc.m_RotationVariance = 0.0
+	desc.m_PositionVariance = 0.0
+	desc.m_SizeRatio = Vector2(1.0, 1.0)
+	desc.m_RandomizeAlpha = false
+	desc.m_PressureSizeRange = Vector2(1.0, 1.0)
+	desc.m_PressureOpacityRange = Vector2(1.0, 1.0)
+	desc.m_Opacity = 1.0
+	desc.m_SizeVariance = 0.0
+
+	var brush := MidpointPlusLifetimeSprayBrush.new()
+	brush.m_BaseSize_PS = 1.0
+	brush.m_Color = Color(0.4, 0.8, 0.2, 1.0)
+	brush.set_random_seed(0)
+	brush.init_brush(desc, TrTransform.identity())
+	brush.set_random_seed(0)
+	return brush
+
+func _expect(condition: bool, label: String) -> void:
+	if not condition:
+		_fail(label)
+
+func _expect_equal(actual: Variant, expected: Variant, label: String) -> void:
+	if actual != expected:
+		_fail("%s expected %s but got %s" % [label, expected, actual])
+
+func _expect_vec3_close(actual: Vector3, expected: Vector3, label: String) -> void:
+	_expect_close(actual.x, expected.x, "%s x" % label)
+	_expect_close(actual.y, expected.y, "%s y" % label)
+	_expect_close(actual.z, expected.z, "%s z" % label)
+
+func _expect_close(actual: float, expected: float, label: String) -> void:
+	if abs(actual - expected) > 1e-5:
+		_fail("%s expected %.8f but got %.8f" % [label, expected, actual])
+
+func _fail(message: String) -> void:
+	_failures += 1
+	push_error("GDSCRIPT_PARITY_MIDPOINTSPRAY: %s" % message)
