cb7cf10c7501bb8790dd3de347f04528a61593f1
Andy Baker <andy@andybak.net>
2026-08-29 17:37:12 +0100

    Pin Icosa addon syntax fix

--- plug.gd
+++ plug.gd
@@ -2,5 +2,6 @@ extends "res://addons/gd-plug/plug.gd"
 
 func _plugging() -> void:
 	plug("https://github.com/icosa-foundation/icosa-godot-addon.git", {
+		"commit": "d567cc8bf56a56b9b64765de653d5490525505e3",
 		"include": ["addons/icosa"],
 	})
